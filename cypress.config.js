d75e887459a829258ef2fdcdcf80a6d3eb992390
Roberto Studart <robertolstudart@gmail.com>
2022-12-15 20:57:10 -0300

    Informações do dashboard do Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: w4pek5,
     baseUrl: 'https://buger-eats-qa.vercel.app',
     setupNodeEvents(on, config) {
       // implement node event listeners here
