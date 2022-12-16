ef89d4a218fcd7c8cba32978ab730db4db85bc8b
Roberto Studart <robertolstudart@gmail.com>
2022-12-15 21:00:32 -0300

    Mudando os padrões do viewport

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    viewportWidth: 1280,
+    viewportHeight: 720,
     projectId: w4pek5,
     baseUrl: 'https://buger-eats-qa.vercel.app',
     setupNodeEvents(on, config) {
