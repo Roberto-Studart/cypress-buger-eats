ff5f7f47ed44ec559a9cc9fed222da3602930a89
Roberto Studart <robertolstudart@gmail.com>
2022-12-15 21:02:13 -0300

    Corrigindo o id do vínculo ao Cypress.io

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     viewportWidth: 1280,
     viewportHeight: 720,
-    projectId: w4pek5,
+    projectId: 'w4pek5',
     baseUrl: 'https://buger-eats-qa.vercel.app',
     setupNodeEvents(on, config) {
       // implement node event listeners here
